4b3570e33a6ff92322a41ca71ee480bfe5ee2c1a
Zain <learnerstudent001@gmail.com>
2023-06-09 22:27:33 +0500

    Admin User Created

--- creating_app.py
+++ creating_app.py
@@ -1,5 +1,6 @@
 # ------------------Creating Project------------------
-'''1. Create a folder for the project
+'''
+1. Create a folder for the project
 run the following command in the terminal:
 django-admin startproject mysite
 now you will see a folder named mysite with a bunch of files in it
@@ -39,6 +40,22 @@ urlpatterns = [
     path("myapp/", include("myapp.urls")),
     path('admin/', admin.site.urls),
 ]
+
+6. Database setup
+run the following command:
+python manage.py migrate
+
+7. Create models
+
+8. Activate models
+add the app name in the INSTALLED_APPS list in settings.py
+run the following command:
+python manage.py makemigrations myapp
+
+in summary:
+-Change your models (in models.py).
+-Run python manage.py makemigrations to create migrations for those changes
+-Run python manage.py migrate to apply those changes to the database.
 
 
 '''
